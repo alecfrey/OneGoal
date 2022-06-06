9d08cfb38d5262adf05bdd15f4e8280e34143130
alecfrey <79166968+alecfrey@users.noreply.github.com>
2022-06-06 10:10:05 -0500

    Initial Commit

---
+++ OneGoal/OneGoalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneGoalApp.swift
+//  OneGoal
+//
+//  Created by Alec Frey on 6/6/22.
+//
+
+import SwiftUI
+
+@main
+struct OneGoalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
